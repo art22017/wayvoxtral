d5cd761743edb4ade9d0dfa8c614d995936c1f29
Artem Korotkov <art22017@yandex.ru>
2026-02-09 08:59:14 +0300

    fix: revert hotkey to F9 as requested

--- src/wayvoxtral/hotkey.py
+++ src/wayvoxtral/hotkey.py
@@ -14,8 +14,8 @@ from evdev import InputDevice, categorize, ecodes
 
 logger = logging.getLogger(__name__)
 
-# F24 - глобальный хоткей для WayVoxtral (mapped from Ctrl+Space by keyd)
-TARGET_KEY = ecodes.KEY_F24
+# F9 - глобальный хоткей для WayVoxtral
+TARGET_KEY = ecodes.KEY_F9
 
 
 class HotkeyListener:
